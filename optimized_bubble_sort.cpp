b07db52ec53059a736c8ddc700849fadd8ad10c4
subham-singh89 <110325911+subham-singh89@users.noreply.github.com>
2023-03-28 14:59:51 +0530

    Create optimized_bubble_sort.cpp

---
+++ optimized_bubble_sort.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+//main bubble sort algoithm
+//not the optimized one
+void bubble_sort(int arr[])
+{
+    int i,j;
+    for(i=0;i<6;i++)
+    {	
+    	int flag=false;
+        for(j=0;j<5;j++)
+        {
+            if(arr[j+1]>arr[j])
+            {
+            	flag=true;
+                int x;
+                x=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=x;
+            }
+            
+        }
+        if(flag==false)
+            {
+            	break;
+			}
+    }
+}
+int main()
+{
+    int myarr[6];
+    cout<<"Enter the elements of the array:"<<endl;
+    for(int i=0;i<6;i++)
+    {
+        cin>>myarr[i];
+    }
+    cout<<"Array before sorting "<<endl;
+    for(int i=0;i<6;i++)
+    {
+        cout<<myarr[i]<<"  ";
+    }
+    cout<<""<<endl;
+    bubble_sort(myarr);
+    cout<<"Array after sorting "<<endl;
+    for(int i=0;i<6;i++)
+    {
+        cout<<myarr[i]<<"  ";
+    }
+
+}
